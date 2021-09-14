b1a5f3ddf81f31a0aab48adbf102a03e5c64c934
Berto Furth <bfgithub@veryspeedy.net>
2021-09-14 17:48:00 +1000

    screen

---
+++ screen/build-screen-02-screen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source build-common
+source build-functions
+check_source_dir "screen"
+change_into_obj_directory
+
+# Account for weird location of ncurses
+export CFLAGS="$CFLAGS -I$BUILDHOST_DEST/$PREFIX/include/ncurses"
+
+configure_it --prefix=$PREFIX \
+	     --bindir=$EXEC_PREFIX/bin \
+	     --sbindir=$EXEC_PREFIX/sbin \
+	     --host=$ARCH
+make_it
+install_it
+finish_it
