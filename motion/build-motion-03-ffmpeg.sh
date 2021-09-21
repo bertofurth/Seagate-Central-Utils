62998d0d4585d48733612a8a0c01f99837c52686
Berto Furth <bfgithub@veryspeedy.net>
2021-09-22 09:07:42 +1000

    Fixed pkg-config

--- motion/build-motion-03-ffmpeg.sh
+++ motion/build-motion-03-ffmpeg.sh
@@ -5,12 +5,15 @@ check_source_dir "ffmpeg"
 change_into_obj_directory
 configure_it --prefix=$PREFIX \
 	     --bindir=$EXEC_PREFIX/bin \
+	     --pkg-config=pkg-config \
 	     --enable-cross-compile \
 	     --target-os=linux \
 	     --arch=$ARCH --cpu=armv6k \
 	     --cross-prefix=$CROSS_COMPILE \
-	     --enable-shared \
-	     --disable-doc 
+	     --enable-gpl \
+	     --enable-version3 \
+	     --enable-gnutls \
+	     --disable-doc
 make_it
 install_it
 finish_it
