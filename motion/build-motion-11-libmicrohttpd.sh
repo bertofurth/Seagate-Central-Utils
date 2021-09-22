fba93215337eb149e492c02b8a6c05719a27ae70
Berto Furth <bfgithub@veryspeedy.net>
2021-09-22 14:09:46 +1000

    Fix problem with accept4()

--- motion/build-motion-11-libmicrohttpd.sh
+++ motion/build-motion-11-libmicrohttpd.sh
@@ -7,10 +7,22 @@ change_into_obj_directory
 # libgnutls.la seems to cause problems. Temporarily
 # make it invisible.
 mv $BUILDHOST_DEST/$PREFIX/lib/libgnutls.la $BUILDHOST_DEST/$PREFIX/lib/libgnutls.la.orig
+
 configure_it --prefix=$PREFIX \
 	     --bindir=$EXEC_PREFIX/bin \
 	     --sbindir=$EXEC_PREFIX/sbin \
 	     --host=$ARCH
+
+# Seagate Central doesn't properly support accept4().
+# I can only guess the old version of libc installed
+# on the Seagate Central doesn't implement it properly.
+#
+# If you don't manually disable libmicrohttpd from
+# using accept4() then the server will not accept
+# any connections.
+#
+sed -i "/HAVE_ACCEPT4/c #undef HAVE_ACCEPT4" $OBJ/$LIB_NAME/MHD_config.h
+
 make_it
 install_it
 # Put libgnutls.la back
