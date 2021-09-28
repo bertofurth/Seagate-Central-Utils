73aa8bb50a97305f024761c8e33187afa90cbb25
Berto Furth <bfgithub@veryspeedy.net>
2021-09-28 19:24:27 +1000

    static linking

--- motion/build-motion-12-alsa-lib.sh
+++ motion/build-motion-12-alsa-lib.sh
@@ -7,10 +7,17 @@ configure_it --prefix=$PREFIX \
 	     --bindir=$EXEC_PREFIX/bin \
 	     --sbindir=$EXEC_PREFIX/sbin \
 	     --host=$ARCH \
-	     --enable-static \
 	     --enable-python2
 
-# N.B. Seagate Central uses python 2
+# If you want static libraries then add
+# the following line to configure. alsa-lib
+# won't let you build both dynamic and static
+# at the same time!!
+#
+#	     --enable-static 
+
+# N.B. Seagate Central uses python 2 so we
+# specify "--enable-python2"
 
 make_it
 install_it
