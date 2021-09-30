d57ef1aafca2ed8efd3284556284bedc80823918
Berto Furth <bfgithub@veryspeedy.net>
2021-09-30 16:40:57 +1000

    Don't build v4l-utils

--- motion/build-motion-13-v4l-utils.sh
+++ motion/build-motion-13-v4l-utils.sh
@@ -3,10 +3,20 @@ source ../build-common
 source ../build-functions
 check_source_dir "v4l-utils"
 change_into_obj_directory
+
+#
+# --disable-v4l-utils
+# v4l-utils don't work on Seagate Central
+# because the version of GLIBCXX on the unit
+# is too old. If you decide to embed a later
+# version of GLIBCXX then hopefully they will
+# work.
+
 configure_it --prefix=$PREFIX \
 	     --bindir=$EXEC_PREFIX/bin \
 	     --sbindir=$EXEC_PREFIX/sbin \
-	     --host=$ARCH 
+	     --host=$ARCH \
+	     --disable-v4l-utils
 make_it
 install_it
 finish_it
