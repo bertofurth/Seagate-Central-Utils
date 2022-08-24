81a0bcdb803bb208561d77c34805373fc156be6f
Berto Furth <bfgithub@veryspeedy.net>
2022-08-24 11:45:59 +1000

    Fixed typo

--- networking/build-networking-43-readline.sh
+++ networking/build-networking-43-readline.sh
@@ -9,7 +9,7 @@ change_into_obj_directory
 configure_it --prefix=$PREFIX \
 	     --bindir=$EXEC_PREFIX/bin \
 	     --sbindir=$EXEC_PREFIX/sbin \
-	     --host=$ARCH \
+	     --host=$ARCH
 make_it
 install_it
 finish_it
