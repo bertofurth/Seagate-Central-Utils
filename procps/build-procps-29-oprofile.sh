f3ab2b7f2e665a9257b99e102f5a0b367e56662a
Berto Furth <bfgithub@veryspeedy.net>
2022-09-03 20:18:40 +1000

    Static link libstdc++

--- procps/build-procps-29-oprofile.sh
+++ procps/build-procps-29-oprofile.sh
@@ -4,10 +4,16 @@ source ../build-functions
 check_source_dir "oprofile"
 
 change_into_obj_directory
+#
+# libstdc++ on the Seagate Central is not
+# recent enough for some of the oprofile tools
+# so we are forced to link libstdc++ statically
+export CXXFLAGS="-static-libstdc++ $CXXFLAGS"
+
 configure_it --prefix=$PREFIX \
 	     --bindir=$EXEC_PREFIX/bin \
 	     --sbindir=$EXEC_PREFIX/sbin \
 	     --host=$ARCH
-make_it
+make_it V=1
 install_it
 finish_it
