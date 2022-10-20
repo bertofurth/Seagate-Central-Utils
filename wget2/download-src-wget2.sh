c4b5b8706301e39218488f56ca8cbed7a4b0631c
Berto Furth <bfgithub@veryspeedy.net>
2022-10-20 17:31:31 +1100

    Minor error in download file

--- wget2/download-src-wget2.sh
+++ wget2/download-src-wget2.sh
@@ -14,7 +14,7 @@ gmp='http://mirrors.kernel.org/gnu/gmp/gmp-6.2.1.tar.xz'
 nettle='http://mirrors.kernel.org/gnu/nettle/nettle-3.7.3.tar.gz'
 libtasn1='http://mirrors.kernel.org/gnu/libtasn1/libtasn1-4.17.0.tar.gz'
 gnutls='https://www.gnupg.org/ftp/gcrypt/gnutls/v3.7/gnutls-3.7.6.tar.xz'
-libgpg-error='https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.46.tar.bz2'
+libgpgerror='https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.46.tar.bz2'
 libassuan='https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.5.tar.bz2'
 gpgme='https://gnupg.org/ftp/gcrypt/gpgme/gpgme-1.18.0.tar.bz2'
 wget2='https://ftp.gnu.org/gnu/wget/wget2-2.0.1.tar.gz'
@@ -25,7 +25,7 @@ echo_archives() {
     echo "${nettle}"
     echo "${libtasn1}"
     echo "${gnutls}"
-    echo "${libgpg-error}"
+    echo "${libgpgerror}"
     echo "${libassuan}"
     echo "${gpgme}"
     echo "${wget2}"
