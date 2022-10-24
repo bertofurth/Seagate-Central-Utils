ed07cf293101b7973f95ff3ee73502e1d49b4431
Berto Furth <bfgithub@veryspeedy.net>
2022-10-25 06:15:55 +1100

    Working on download script

--- lighttpd/download-src-lighttpd.sh
+++ lighttpd/download-src-lighttpd.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
+source ../common/download-common
 
 # Run this script to download and extract the versions
-# of source code this project was tested with. Unless
-# otherwise noted these are the latest stable versions
-# available at the time of writing.
-
-# Based on gcc's download_prerequisites script
+# of source code this project was tested with. In general
+# these are the latest stable versions at the time of
+# writing.
 
 ncurses='http://mirrors.kernel.org/gnu/ncurses/ncurses-6.2.tar.gz'
 gmp='http://mirrors.kernel.org/gnu/gmp/gmp-6.2.1.tar.xz'
@@ -39,30 +38,4 @@ echo_archives() {
     echo "${lighttpd}"
 }
 
-die() {
-    echo "error: $@" >&2
-    exit 1
-}
-
-mkdir -p src
-cd src
-
-if type wget > /dev/null ; then
-    fetch='wget --backups=1'
-else
-    if type curl > /dev/null; then
-	fetch='curl -LO'
-    else
-	die "Unable to find wget or curl"
-    fi    
-fi
-
-
-for ar in $(echo_archives)
-do
-	${fetch} "${ar}"    \
-		 || die "Cannot download $ar"
-        tar -xf "$(basename ${ar})" \
-		 || die "Cannot extract $(basename ${ar})"
-done
-unset ar
+do_download $(echo_archives)
