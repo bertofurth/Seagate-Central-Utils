da6f061d9f627c603889cf0aedd45559df76fc58
Berto Furth <bfgithub@veryspeedy.net>
2022-10-25 12:02:35 +1100

    nettle

---
+++ libs/build-nettle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Prerequisites for nettle
+# gmp
+
+source ../build-common
+source ../build-functions
+check_source_dir "nettle"
+change_into_obj_directory
+configure_it --prefix=$PREFIX \
+	     --bindir=$EXEC_PREFIX/bin \
+	     --sbindir=$EXEC_PREFIX/sbin \
+	     --host=$ARCH 
+make_it
+install_it
+finish_it
