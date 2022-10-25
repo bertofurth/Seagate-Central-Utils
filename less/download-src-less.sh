ff0ea3305dda9dccf23c9424f1d6f485a5bb4a63
Berto Furth <bfgithub@veryspeedy.net>
2022-10-25 22:15:56 +1100

    Convert less to new format

--- less/download-src-less.sh
+++ less/download-src-less.sh
@@ -1,44 +1,13 @@
 #!/bin/sh
+source ../common/download-common
 
 # Run this script to download and extract the versions
-# of source code this project was tested with. Unless
-# otherwise noted these are the latest stable versions
-# available at the time of writing.
+# of source code this project was tested with. In general
+# these are the latest stable versions at the time of
+# writing.
 
-# Based on gcc's download_prerequisites script
+# Download source files specific to this project
+do_download http://mirrors.kernel.org/gnu/less/less-590.tar.gz
 
-ncurses='http://mirrors.kernel.org/gnu/ncurses/ncurses-6.2.tar.gz'
-less='http://mirrors.kernel.org/gnu/less/less-590.tar.gz'
-
-echo_archives() {
-    echo "${ncurses}"
-    echo "${less}"
-}
-
-die() {
-    echo "error: $@" >&2
-    exit 1
-}
-
-mkdir -p src
-cd src
-
-if type wget > /dev/null ; then
-    fetch='wget --backups=1'
-else
-    if type curl > /dev/null; then
-	fetch='curl -LO'
-    else
-	die "Unable to find wget or curl"
-    fi    
-fi
-
-
-for ar in $(echo_archives)
-do
-	${fetch} "${ar}"    \
-		 || die "Cannot download $ar"
-        tar -xf "$(basename ${ar})" \
-		 || die "Cannot extract $(basename ${ar})"
-done
-unset ar
+# Download common libraries
+../libs/download-src-ncurses.sh
