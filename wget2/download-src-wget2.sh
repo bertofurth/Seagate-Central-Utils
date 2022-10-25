3f391c15c53a31cf5addda3d8812386eac49cf1b
Berto Furth <bfgithub@veryspeedy.net>
2022-10-26 06:36:48 +1100

    Fix download wget2

--- wget2/download-src-wget2.sh
+++ wget2/download-src-wget2.sh
@@ -7,15 +7,10 @@ source ../common/download-common
 # writing.
 
 # Download source files specific to this project
-do_download
-
-
-libtasn1='http://mirrors.kernel.org/gnu/libtasn1/libtasn1-4.17.0.tar.gz'
-gnutls='https://www.gnupg.org/ftp/gcrypt/gnutls/v3.7/gnutls-3.7.6.tar.xz'
-libgpgerror='https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.46.tar.bz2'
-libassuan='https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.5.tar.bz2'
-gpgme='https://gnupg.org/ftp/gcrypt/gpgme/gpgme-1.18.0.tar.bz2'
-wget2='https://ftp.gnu.org/gnu/wget/wget2-2.0.1.tar.gz'
+do_download https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.46.tar.bz2
+do_download https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.5.tar.bz2
+do_download https://gnupg.org/ftp/gcrypt/gpgme/gpgme-1.18.0.tar.bz2
+do_download https://ftp.gnu.org/gnu/wget/wget2-2.0.1.tar.gz
 
 if [[ -n $SKIP_COMMON ]]; then
     exit 0
@@ -25,3 +20,5 @@ fi
 ../libs/download-src-zlib.sh
 ../libs/download-src-gmp.sh
 ../libs/download-src-nettle.sh
+../libs/download-src-libtasn1.sh
+../libs/download-src-gnutls.sh
