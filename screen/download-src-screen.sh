5fd302dbdc1466a8829c78fb6eba7505f088d1e1
Berto Furth <bfgithub@veryspeedy.net>
2022-10-25 23:03:44 +1100

    screen new format

--- screen/download-src-screen.sh
+++ screen/download-src-screen.sh
@@ -1,44 +1,18 @@
 #!/bin/sh
+source ../common/download-common
 
 # Run this script to download and extract the versions
-# of source code this project was tested with. Unless
-# otherwise noted these are the latest stable versions
-# available at the time of writing.
+# of source code this project was tested with. In general
+# these are the latest stable versions at the time of
+# writing.
 
-# Based on gcc's download_prerequisites script
+# Download source files specific to this project
+do_download http://mirrors.kernel.org/gnu/screen/screen-4.8.0.tar.gz
 
-ncurses='http://mirrors.kernel.org/gnu/ncurses/ncurses-6.2.tar.gz'
-screen='http://mirrors.kernel.org/gnu/screen/screen-4.8.0.tar.gz'
 
-echo_archives() {
-    echo "${ncurses}"
-    echo "${screen}"
-}
-
-die() {
-    echo "error: $@" >&2
-    exit 1
-}
-
-mkdir -p src
-cd src
-
-if type wget > /dev/null ; then
-    fetch='wget --backups=1'
-else
-    if type curl > /dev/null; then
-	fetch='curl -LO'
-    else
-	die "Unable to find wget or curl"
-    fi    
+if [[ -n $SKIP_COMMON ]]; then
+    exit 0
 fi
 
-
-for ar in $(echo_archives)
-do
-	${fetch} "${ar}"    \
-		 || die "Cannot download $ar"
-        tar -xf "$(basename ${ar})" \
-		 || die "Cannot extract $(basename ${ar})"
-done
-unset ar
+# Download common libraries
+../libs/download-src-ncurses.sh
