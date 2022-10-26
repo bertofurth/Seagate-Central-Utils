8bdb61c7724e80c4670e5e54ec3a37cb0e9bfaa0
Berto Furth <bfgithub@veryspeedy.net>
2022-10-27 10:40:55 +1100

    Fixing build-perl

--- perl/build-perl-90-perl.sh
+++ perl/build-perl-90-perl.sh
@@ -3,10 +3,14 @@ source ../common/build-common
 source ../common/build-functions
 check_source_dir "perl"
 change_into_obj_directory
+
+#
+# N.B. Configure options for perl-cross are a little
+# non-standard.
+#
 configure_it --prefix=$PREFIX \
-	     --bindir=$EXEC_PREFIX/bin \
-	     --sbindir=$EXEC_PREFIX/sbin \
-	     --host=$ARCH
+	     --target=$ARCH \
+	     --target-tools-prefix=$CROSS_COMPILE
 make_it
 install_it
 finish_it
