ac8d5c98f57cf2dc4d9c244ed5b2e9032f6924be
Berto Furth <bfgithub@veryspeedy.net>
2022-10-27 11:36:04 +1100

    Refine perl

--- perl/build-perl-90-perl.sh
+++ perl/build-perl-90-perl.sh
@@ -6,6 +6,7 @@ source ../common/build-functions
 # Cross compiling perl is quite an unusual process
 # See https://arsv.github.io/perl-cross/ for the details
 
+TEMP_PERL_DIR=temp-perl
 # First find the perl-cross- directory
 # and save the location
 
@@ -13,14 +14,28 @@ check_source_dir "perl-cross"
 PERL_CROSS_SRC=${SRC}/${LIB_NAME}
 unset LIB_NAME
 
-# Now find the actual perl-5 directory
+# Now find the actual perl-5 directory and
+# save this location
 check_source_dir "perl-5"
+PERL_5_SRC=${SRC}/${LIB_NAME}
+unset LIB_NAME
+
+# Now create a new directory with the
+# combined contents of the perl-5 source
+# directory with perl-cross copied over it
 
 # Copy the contents of the perl-cross
 # directory over the top of the perl
 # source directory.
-echo Copying contents of ${PERL_CROSS_SRC} over ${SRC}/${LIB_NAME}
-cp -r $PERL_CROSS_SRC/* ${SRC}/${LIB_NAME}/
+mkdir -p ${SRC}/${TEMP_PERL_DIR}
+check_source_dir ${TEMP_PERL_DIR}
+# Clear out the directory. Build doesn't work
+# well if there's stuff in there already
+rm -rf .
+echo Copying contents of ${PERL_5_SRC} to ${SRC}/${LIB_NAME}
+cp -r ${PERL_5_SRC}/* .
+echo Copying contents of ${PERL_CROSS_SRC} to ${SRC}/${LIB_NAME}
+cp -r ${PERL_CROSS_SRC}/* .
 
 # Build works better in tree
 #change_into_obj_directory
